f63041da352a96c2f1c26db78833ae106445ad7e
Jean Piere Dudey <jeandudey@hotmail.com>
2016-02-08 10:19:51 -0430

    fix(lint): Remove unstable_features lint.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![deny(missing_debug_implementations, missing_copy_implementations,
         trivial_casts, trivial_numeric_casts,
         unsafe_code,
-        unstable_features,
         unused_import_braces, unused_qualifications)]
 
 #![cfg_attr(feature="dev", feature(plugin))]
